1b241bdc7170ccb450da78ca98a36e2a2a9e3323
T0chka <an.dolgorukova@gmail.com>
2025-02-01 19:15:23 +0300

    UPD: visual representation

--- static/task_schematic.py
+++ static/task_schematic.py
@@ -12,70 +12,93 @@ def calculate_positions(center, radius, num_circles, start_angle):
         positions.append((x, y))
     return positions
 
-def draw_rounded_rectangle_chain(ax, center, puzzle_width, num_circles, highlight_indices, circle_radius, fontsize_numbers, highlight_color):
-    """
-    Draws a chain of circles in the form of a rectangle with rounded corners
-    """
-    w = puzzle_width/3  # width of the rectangle
-    h = puzzle_width/4  # height of the rectangle
-    r = puzzle_width/15  # radius of corners
-    
-    # Calculate the perimeter
-    perimeter_horizontal = w - 2 * r
-    perimeter_vertical = h - 2 * r
-    arc_length = np.pi * r / 2
-    total_perimeter = 2 * (perimeter_horizontal + perimeter_vertical) + 4 * arc_length
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
-    # Number of circles along the perimeter
-    n_total = num_circles
-    
-    def generate_points(start, length, n, is_arc=False, center=None, radius=None, start_angle=None, end_angle=None):
-        if is_arc:
-            angles = np.linspace(start_angle, end_angle, n, endpoint=False)
-            return center[0] + radius * np.cos(angles), center[1] + radius * np.sin(angles)
-        else:
-            n = int(n)
-            return np.linspace(start[0], start[0] + length, n, endpoint=False), np.full(n, start[1])
-    
-    # Calculate all segments
-    x_top, y_top = generate_points((center[0] - w/2 + r, center[1] + h/2), perimeter_horizontal, 
-                                 int(n_total * perimeter_horizontal / total_perimeter))
-    
-    x_tr, y_tr = generate_points(None, None, int(n_total * arc_length / total_perimeter),
-        is_arc=True, center=(center[0] + w/2 - r, center[1] + h/2 - r), 
-        radius=r, start_angle=np.pi/2, end_angle=0)
-    
-    x_right, y_right = generate_points((center[0] + w/2, center[1] + h/2 - r), -perimeter_vertical,
-                                     int(n_total * perimeter_vertical / total_perimeter))
-    
-    x_br, y_br = generate_points(None, None, int(n_total * arc_length / total_perimeter),
-        is_arc=True, center=(center[0] + w/2 - r, center[1] - h/2 + r),
-        radius=r, start_angle=0, end_angle=-np.pi/2)
-    
-    x_bottom, y_bottom = generate_points((center[0] + w/2 - r, center[1] - h/2), -perimeter_horizontal,
-                                       int(n_total * perimeter_horizontal / total_perimeter))
-    
-    x_bl, y_bl = generate_points(None, None, int(n_total * arc_length / total_perimeter),
-        is_arc=True, center=(center[0] - w/2 + r, center[1] - h/2 + r),
-        radius=r, start_angle=-np.pi/2, end_angle=-np.pi)
-    
-    x_left, y_left = generate_points((center[0] - w/2, center[1] - h/2 + r), perimeter_vertical,
-                                   int(n_total * perimeter_vertical / total_perimeter))
-    
-    x_tl, y_tl = generate_points(None, None, int(n_total * arc_length / total_perimeter),
-        is_arc=True, center=(center[0] - w/2 + r, center[1] + h/2 - r),
-        radius=r, start_angle=-np.pi, end_angle=-3*np.pi/2)
-    
-    # Combine all coordinates
-    xs = np.concatenate([x_top, x_tr, x_right, x_br, x_bottom, x_bl, x_left, x_tl])
-    ys = np.concatenate([y_top, y_tr, y_right, y_br, y_bottom, y_bl, y_left, y_tl])
-    
-    # Draw circles
-    for i, (x, y) in enumerate(zip(xs, ys)):
+def draw_rounded_rectangle_chain(ax, center, puzzle_width, num_circles, 
+                                 highlight_indices, circle_radius, 
+                                 fontsize_numbers, highlight_color):
+    cx, cy = center
+    base_width = puzzle_width / 3
+    h = puzzle_width / 4
+    r = puzzle_width / 15
+
+    w = base_width + (num_circles - 10) * circle_radius * 2
+
+    x_left, x_right = cx - w / 2, cx + w / 2
+    y_top, y_bottom = cy + h / 2, cy - h / 2
+
+    if num_circles < 8:
+        raise ValueError("Минимум 8 кружков")
+
+    top_extra = (num_circles - 8) // 2
+    bottom_extra = num_circles - 8 - top_extra
+
+    coords = []
+
+    coords.append((x_left + r, y_top))
+    if top_extra > 0:
+        spacing_top = (x_right - r - (x_left + r)) / (top_extra + 1)
+        coords.extend([(x_left + r + i * spacing_top, y_top) for i in range(1, top_extra + 1)])
+    coords.append((x_right - r, y_top))
+    coords.extend([(x_right, y) for y in np.linspace(y_top - r, y_bottom + r, 2)])
+    coords.append((x_right - r, y_bottom))
+    if bottom_extra > 0:
+        spacing_bottom = (x_right - r - (x_left + r)) / (bottom_extra + 1)
+        coords.extend([(x_left + r + i * spacing_bottom, y_bottom) for i in range(1, bottom_extra + 1)])
+    coords.append((x_left + r, y_bottom))
+    coords.extend([(x_left, y) for y in np.linspace(y_bottom + r, y_top - r, 2)])
+    coords.append((x_left + r, y_top))
+
+    coords = coords[:num_circles]
+
+    for i, (x, y) in enumerate(coords):
         color = highlight_color if i in highlight_indices else 'white'
         circle = Circle((x, y), radius=circle_radius, edgecolor='black', facecolor=color)
         ax.add_patch(circle)
-        ax.text(x, y, str(i+1), ha='center', va='center', fontsize=fontsize_numbers)
+        ax.text(x, y, str(i + 1), ha='center', va='center', fontsize=fontsize_numbers)
+# def draw_rounded_rectangle_chain(ax, center, puzzle_width, num_circles, 
+#                                  highlight_indices, circle_radius, 
+#                                  fontsize_numbers, highlight_color):
+#     cx, cy = center
+#     base_width = puzzle_width / 3
+#     h = puzzle_width / 4
+#     r = puzzle_width / 15
+
+#     w = base_width + (num_circles - 8) * circle_radius
+
+#     x_left, x_right = cx - w / 2, cx + w / 2
+#     y_top, y_bottom = cy + h / 2, cy - h / 2
+
+#     top_extra = (num_circles - 8) // 2
+#     bottom_extra = num_circles - 8 - top_extra
+
+#     coords = []
+
+#     coords.append((x_right - r, y_top))
+#     coords.extend([(x_right, y) for y in np.linspace(y_top - r, y_bottom + r, 2)])
+#     coords.append((x_right - r, y_bottom))
+
+#     if bottom_extra > 0:
+#         spacing_bottom = (x_right - r - (x_left + r)) / (bottom_extra + 1)
+#         coords.extend([(x_left + r + i * spacing_bottom, y_bottom) for i in range(1, bottom_extra + 1)])
+
+#     coords.append((x_left + r, y_bottom))
+#     coords.extend([(x_left, y) for y in np.linspace(y_bottom + r, y_top - r, 2)])
+#     coords.append((x_left + r, y_top))
+
+#     if top_extra > 0:
+#         spacing_top = (x_right - r - (x_left + r)) / (top_extra + 1)
+#         coords.extend([(x_left + r + i * spacing_top, y_top) for i in range(1, top_extra + 1)])
+
+#     coords = coords[:num_circles]
+
+#     for i, (x, y) in enumerate(coords):
+        color = highlight_color if i in highlight_indices else 'white'
+        circle = Circle((x, y), radius=circle_radius, edgecolor='black', facecolor=color)
+        ax.add_patch(circle)
+        ax.text(x, y, str(i + 1), ha='center', va='center', fontsize=fontsize_numbers)
 
 def draw_three_puzzles():
     # Create a common field
@@ -99,16 +122,15 @@ def draw_three_puzzles():
     title_y = puzzle_width / 2
 
     ##################################################################
-    # 1. LRX (2 circles swap)
+    # 1. LRX (generates 10 circles)
     ##################################################################
-
     center_lrx = centers[0]
     ax.text(center_lrx[0], title_y, "LRX", fontsize=fontsize_title, ha='center')
     draw_rounded_rectangle_chain(
         ax = ax,
         center = center_lrx,
         puzzle_width = puzzle_width,
-        num_circles = 16,
+        num_circles = 12,
         highlight_indices = {1, 2},
         circle_radius = circle_radius,
         fontsize_numbers = fontsize_numbers,
@@ -116,7 +138,7 @@ def draw_three_puzzles():
     )
     
     ##################################################################
-    # 2. TopSpin
+    # 2. TopSpin (generates 12 circles)
     ##################################################################
     center_topspin = centers[1]
     ax.text(center_topspin[0], title_y, "TopSpin", fontsize=fontsize_title, ha='center')
@@ -125,7 +147,7 @@ def draw_three_puzzles():
         center = center_topspin,
         puzzle_width = puzzle_width,
         num_circles = 16,
-        highlight_indices = {0, 1, 2, 3},
+        highlight_indices = {1, 2, 3, 4},
         circle_radius = circle_radius,
         fontsize_numbers = fontsize_numbers,
         highlight_color = highlight_color
@@ -138,45 +160,42 @@ def draw_three_puzzles():
     ax.text(center_hungarian[0], title_y, "Hungarian Rings", fontsize=fontsize_title, ha='center')
 
     # Ring parameters
-    R = puzzle_width / 4  # Radius of the rings
+    R = 0.8
     num_circles_hungarian = 8
     start_angle_ring1 = 0
     start_angle_ring2 = 0
-    i, j = 1, 3  # indices of the circles to overlap
+    i, j = 1, 3 # indices of the circles to overlap
 
-    # Calculate centers of rings
+    # Solve for d given the angles
     theta1 = np.radians(start_angle_ring1 + i*(360/num_circles_hungarian))
     theta2 = np.radians(start_angle_ring2 + j*(360/num_circles_hungarian))
     d = -R*(np.cos(theta2) - np.cos(theta1))/2
 
     # Set the centers of the rings
-    center_ring1 = (center_hungarian[0] - d, 0)
-    center_ring2 = (center_hungarian[0] + d, 0)
+    center_ring1 = (center_hungarian[0] - d, center_hungarian[1])
+    center_ring2 = (center_hungarian[0] + d, center_hungarian[1])
 
     # Calculate positions for both rings
-    positions_ring1 = calculate_positions(center_ring1, R, num_circles_hungarian, start_angle_ring1)
-    positions_ring2 = calculate_positions(center_ring2, R, num_circles_hungarian, start_angle_ring2)
+    positions_ring1 = calculate_positions(center_ring1, R, num_circles_hungarian, 0)
+    positions_ring2 = calculate_positions(center_ring2, R, num_circles_hungarian, 0)
 
-    # Define numbering for the circles
+    # Define numbering
     numbers_ring1 = [2, 3, 4, 5, 6, 7, 8, 1]
     numbers_ring2 = [11, 12, 13, 3, 15, 1, 9, 10]
 
-    # Draw first ring
+    # Draw first ring (red)
     for idx, (x, y) in enumerate(positions_ring1):
         circle = Circle((x, y), circle_radius, edgecolor='black', facecolor='lightcoral')
         ax.add_patch(circle)
-        ax.text(x, y, str(numbers_ring1[idx]),
-                color='black', ha='center', va='center', fontsize=10)
+        ax.text(x, y, str(numbers_ring1[idx]), ha='center', va='center', fontsize=fontsize_numbers)
 
-    # Draw second ring
+    # Draw second ring (green)
     for idx, (x, y) in enumerate(positions_ring2):
         circle = Circle((x, y), circle_radius, edgecolor='black', facecolor=highlight_color)
         ax.add_patch(circle)
-        ax.text(x, y, str(numbers_ring2[idx]),
-                color='black', ha='center', va='center', fontsize=10)
+        ax.text(x, y, str(numbers_ring2[idx]), ha='center', va='center', fontsize=fontsize_numbers)
 
-    # Set the limits
-    ax.set_xlim(-1, 18)
+    ax.set_xlim(-1, plot_width + 1)
     ax.set_ylim(-2, 3)
     plt.tight_layout()
     plt.show()
